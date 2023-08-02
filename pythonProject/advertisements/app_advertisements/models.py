ed970b2e007fe34c2b6e7f97594ad95aa3319bfa
Dasha Kameneva <d451255@gmail.com>
2023-08-02 10:37:03 +0300

    домашкааааа

--- pythonProject/advertisements/app_advertisements/models.py
+++ pythonProject/advertisements/app_advertisements/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Advertisement(models.Model):
+    first_name = models.CharField("Заголовок", max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметье, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__(self):
+        return f"id={self.id}, first_name={self.first_name}, price={self.price}"
